aa95ae5e346c99e6aded43187553a3c559dbb1e5
Abis222 <110358766+Abis222@users.noreply.github.com>
2026-03-28 03:38:17 +0300

    Update NEMO.lua

--- NEMO.lua
+++ NEMO.lua
@@ -1,27 +1,21 @@
--- استدعاء مكتبة Redz الأصلية
 local RedzLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/real-redz/RedzLibV5/main/Source.lua"))()
 
--- إنشاء النافذة بنفس اسم سكربتك وحقوقك
 local Window = RedzLib:MakeWindow({
   Title = "NEMO HUB",
   SubTitle = "by Abbas",
+  Icon = "rbxassetid://15298567397",
   SaveFolder = "NemoConfig.json"
 })
 
--- إضافة الزر الدائري (NO) لإخفاء وإظهار القائمة
 Window:AddMinimizeButton({
   ButtonText = "NO",
   Description = "NEMO HUB Toggle",
   Icon = "rbxassetid://15298567397" 
 })
 
--- إنشاء قسم Main (الوحيد حالياً)
 local MainTab = Window:MakeTab({
   Name = "Main",
   Icon = "rbxassetid://4483345998"
 })
 
--- ترحيب داخل القسم لكي تتأكد أنه اشتغل
 MainTab:AddSection({"Welcome Abbas to NEMO HUB"})
-
-MainTab:AddParagraph("Status","The script is now working perfectly!")
